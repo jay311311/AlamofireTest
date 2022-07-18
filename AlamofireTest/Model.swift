a7235e927ff87acf57b429c95c3867dc482633ae
jay311311 <jay311311@gmail.com>
2022-07-18 20:46:35 +0900

    Alamofire practice : baseURL + path

---
+++ AlamofireTest/Model.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  AlamofireTest
+//
+//  Created by pineone on 2022/07/18.
+//
+
+import Foundation
+
+struct NewBook:Codable {
+    var books : [Books]
+}
+
+struct SearchBook:Codable{
+    var books : [Books]
+}
+
+struct Books:Codable, Hashable{
+    var title : String
+    var subtitle : String
+    var isbn13 : String
+    var price : String
+    var image : String
+    var url:String
+}
+
+struct DetailBook:Codable{
+    var title : String
+    var subtitle :String
+    var isbn13: String
+    var price : String
+    var image : String
+    var url:String
+}
--- AlamofireTest/ViewController.swift
+++ AlamofireTest/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchDataPath()
+    }
+    
+    func fetchDataBasic(){
+        let  request = AF.request("https://api.itbook.store/1.0/new" )
+        request.validate(statusCode: 200..<500)
+        .responseDecodable(of:NewBook.self) { data in
+            switch data.result{
+            case .success:
+                print("\(String(describing: data.value))")
+            case .failure(_):
+                print("\(String(describing: data.error?.errorDescription))")
+            }
+        }
+    
+    }
+    
+    func fetchDataPath(_ isbn :String = "1001656317756"){
+        let  baseURL =  URL(string: "https://api.itbook.store/1.0/books")!
+        let url = baseURL.appendingPathComponent(isbn)
+        let request  =  AF.request(url)
+        request.validate(statusCode: 200..<500)
+        .responseDecodable(of:DetailBook.self) { data in
+            switch data.result{
+            case .success:
+                print("detail : \(String(describing: data.value))")
+            case .failure(_):
+                print("detail : \(String(describing: data.error?.errorDescription))")
+            }
+        }
     }
 
 
 }
+
+
 
